ad9ffd7aca3cb50632e8c16c84d2904b32ffd82d
Zielinski Casper <casper.zielinski.endercasper@gmail.com>
2025-05-11 17:49:06 +0200

    finished LeetCode easy Question | exc. Extension

--- src/main/java/at/fhj/msd/App.java
+++ src/main/java/at/fhj/msd/App.java
@@ -1,10 +1,19 @@
 package at.fhj.msd;
 
+import java.lang.annotation.Target;
+import java.util.Arrays;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int[] v = {1,2,3,4,6,10};
+        int target = 10;
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.twoSum(v, 4)));        
+        
+
     }
 }
