7cfbfef53e7c06ede25f6482ca2c322b6826f540
casper-zielinski <casper.zielinski@edu.fh-joanneum.at>
2026-03-25 22:32:58 +0100

    feat: Better Solution for Remove Duplicates

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -2,22 +2,17 @@ import java.util.Arrays;
 
 public class RemoveDuplicates {
     public int removeDuplicates(int[] nums) {
-        int k = 1;
-        int i = 0;
-        int lastUnique = nums[i];
-        while (nums.length - 1 > i) {
-            while (nums.length - 1 > i && lastUnique == nums[i]) {
-                i++;
+        int j = 1;
+        int i = 1;
+        while (nums.length > i) {
+            if (nums[i - 1] != nums[i]) {
+                nums[j] = nums[i];
+                ++j;
             }
-            if (lastUnique != nums[i]) {
-                lastUnique = nums[i];
-                nums[k] = lastUnique;
-                k++;
 
-            }
+            i++;
         }
 
-        System.out.println(Arrays.toString(nums));
-        return k;
+        return j;
     }
 }
