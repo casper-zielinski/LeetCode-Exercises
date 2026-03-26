0a266d7f63a1986d3ebef9fae736f0471aa66339
casper-zielinski <casper.zielinski@edu.fh-joanneum.at>
2026-03-26 20:41:37 +0100

    refactor: every method solution to solution class

--- Solution.java
+++ Solution.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
     public int removeElement(int[] nums, int val) {
@@ -16,5 +18,42 @@ public class Solution {
 
         System.out.println(Arrays.toString(nums));
         return n;
+    }
+
+    public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<>();
+        int i = 0;
+
+        while (i < nums.length) {
+            int start = nums[i];
+
+            while (i < nums.length - 1 && nums[i] + 1 == nums[i + 1]) {
+                i++;
+            }
+
+            if (start == nums[i]) {
+                res.add(String.valueOf(start));
+            } else {
+                res.add(String.format("%d->%d", start, nums[i]));
+            }
+            i++;
+        }
+
+        return res;
+    }
+
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        int i = 1;
+        while (nums.length > i) {
+            if (nums[i - 1] != nums[i]) {
+                nums[j] = nums[i];
+                ++j;
+            }
+
+            i++;
+        }
+
+        return j;
     }
 }
--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = {3,3};
+        int[] nums = {1,3};
         System.out.println(solution.removeElement(nums, 5));
     }
 }
