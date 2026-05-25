aa2f252efb479c47a7f458d08ae62080f2b5cb92
casper-zielinski <casper.zielinski@edu.fh-joanneum.at>
2026-05-25 20:41:42 +0200

    feat: solved 1871. Jump Game VII (with help)

--- Solution.java
+++ Solution.java
@@ -2,6 +2,39 @@ import java.util.*;
 
 public class Solution {
 
+    public boolean canReach(String s, int minJump, int maxJump) {
+        int n = s.length();
+        if (s.charAt(n - 1) == '1')
+            return false;
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        boolean[] visited = new boolean[n];
+        visited[0] = true;
+
+        int farthest = 0;
+
+        while (!queue.isEmpty()) {
+            int i = queue.poll();
+
+            int start = Math.max(i + minJump, farthest + 1);
+            int end = Math.min(i + maxJump, n - 1);
+
+            for (int j = start; j <= end; j++) {
+                if (s.charAt(j) == '0' && !visited[j]) {
+                    if (j == n - 1)
+                        return true;
+                    visited[j] = true;
+                    queue.add(j);
+                }
+            }
+
+            farthest = Math.max(farthest, end);
+        }
+
+        return false;
+    }
+
     public int maxJumps(int[] arr, int d) {
         int[] memo = new int[arr.length];
         int ans = 1;
