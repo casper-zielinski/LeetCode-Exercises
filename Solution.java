f6a54ec78914e40bef65f7bd6e364a7b1159056a
casper-zielinski <casper.zielinski@edu.fh-joanneum.at>
2026-06-05 20:01:42 +0200

    feat: solved 2807. Insert Greatest Common Divisiors in Linked List

--- Solution.java
+++ Solution.java
@@ -2,13 +2,36 @@ import java.util.*;
 
 public class Solution {
 
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        ListNode curr = head;
+
+        while (curr.next != null) {
+            ListNode nextNode = curr.next;
+
+            int valToInsert = Math.min(curr.val, nextNode.val);
+
+            while (valToInsert > 1) {
+                if (curr.val % valToInsert == 0 && nextNode.val % valToInsert == 0) {
+                    break;
+                } else {
+                    valToInsert--;
+                }
+            }
+
+            curr.next = new ListNode(valToInsert, nextNode);
+            curr = nextNode;
+        }
+
+        return head;
+    }
+
 
     public ListNode deleteDuplicates(ListNode head) {
         ListNode curr = head;
 
         while (curr != null) {
             if (curr.next != null && curr.val == curr.next.val) {
-                    curr.next = curr.next.next;
+                curr.next = curr.next.next;
             } else {
                 curr = curr.next;
             }
