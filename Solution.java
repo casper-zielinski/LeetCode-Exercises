6b4bf9b825dd8054048dd1978db87a392d0f8bca
casper-zielinski <casper.zielinski@edu.fh-joanneum.at>
2026-06-16 21:14:59 +0200

    feat: solved 21. Merge Two Sorted Lists
    solved 21. Merge Two Sorted Lists beating 100.00% in Runtime and 37.97% in Memory

--- Solution.java
+++ Solution.java
@@ -2,6 +2,42 @@ import java.util.*;
 
 public class Solution {
 
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 != null) {
+            return list2;
+        } else if (list1 != null && list2 == null) {
+            return list1;
+        } else if (list1 == null && list2 == null) {
+            return null;
+        }
+
+        ListNode curr = new ListNode();
+        ListNode ans = curr;
+
+        curr.val = Math.min(list1.val, list2.val);
+
+        if (list1.val < list2.val) {
+            list1 = list1.next;
+        } else {
+            list2 = list2.next;
+        }
+
+        while (list1 != null || list2 != null) {
+
+            if (list2 == null || list1 != null && list1.val < list2.val) {
+                curr.next = list1;
+                curr = curr.next;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                curr = curr.next;
+                list2 = list2.next;
+            }
+        }
+
+        return ans;
+    }
+
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode cur = head;
