8d58e8eb2a31a60af05f77e710c2e6e0a3302eeb
casper-zielinski <casper.zielinski@edu.fh-joanneum.at>
2026-06-16 21:40:35 +0200

    feat: solved 876. Middle of Linked List
    solved 876. Middle of Linked List beating 100.00% in Runtime and 96.37% in Memory

--- Solution.java
+++ Solution.java
@@ -2,6 +2,18 @@ import java.util.*;
 
 public class Solution {
 
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
